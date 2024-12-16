ea5d97e91cdd0192a6b6c2f3b90ffd5cdf562e9a
marilenasantarossa <marilena.santarossa04@gmail.com>
2024-12-16 13:13:15 +0100

    modifiche incertezze

--- mari/minimi.py
+++ mari/minimi.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 from scipy import stats
+from scipy.stats import norm
 
 parser = argparse.ArgumentParser(description="Plot data da un file specificato")
 parser.add_argument('filename', type=str, help='nome del file da leggere')
@@ -43,9 +44,15 @@ s_a =np.sqrt(cov_matrix[0, 0])
 s_b =np.sqrt(cov_matrix[1, 1])
 minimo = -b/(2*a)
 s_minimo = minimo*np.sqrt(s_b**2/b**2 + s_a**2/4*a**2)
-print(f"theta(-1)= {minimo} +- {s_minimo}")
+print(f"theta(1)= {minimo} +- {s_minimo}")
 print(f"a={a} +- {s_a}")
 print(f"b={b} +- {s_b}")
+
+#metodo massima verosimiglianza
+s_a1=np.sqrt(1/(np.sum(passi_motore**4/(0.0001*intensity)**2)))
+s_b1=np.sqrt(1/(np.sum(passi_motore**2/(0.0001*intensity)**2)))
+s_minimo1 = minimo*np.sqrt(s_b1**2/b**2 + s_a1**2/4*a**2)
+print(f"theta(1) max verosimiglianza= {minimo} +- {s_minimo1}")
 
 plt.scatter(passi_motore, intensity)
 plt.scatter(minimo, polynomial(minimo), color="green")
@@ -55,4 +62,23 @@ plt.xlabel("Passi Motore")
 plt.ylabel("Intensity")
 plt.title("Intensità vs Passi Motore")
 plt.show()
+
+s_y= np.sqrt(1/np.size(intensity)*np.sum((intensity-polynomial(passi_motore))**2))
+chi= np.sum((intensity-(a*passi_motore**2 + b*passi_motore + c))**2/s_y**2)
+
+print(f"s_y ={s_y} \n")
+print(f"chi={chi} , GDL= {np.size(intensity)-3}\n")
+
+#conversione passi motore radiante
+minimo_rad= minimo*0.019 #mrad
+s_conversione = 0.019*np.sqrt((0.005/0.5)**2 + (0.5/(400*65.5))**2)
+s_minimorad=np.sqrt(0.019**2*s_minimo**2 + minimo**2*s_conversione**2)
+s_minimorad1=np.sqrt(0.019**2*s_minimo1**2 + minimo**2*s_conversione**2)
+
+
+print(f"minimo radianti: {minimo_rad} +- {s_minimorad} mrad \n")
+print(f"minimo radianti verosimiglianza: {minimo_rad} +- {s_minimorad1} mrad \n")
+print(f"s_conv={s_conversione}\n")
+
+
 
